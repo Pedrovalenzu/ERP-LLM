2d74db146c5bb0325a9650d96c6c080843350843
Pedro <Pedrovaleji@gmail.com>
2026-05-15 12:22:05 +0200

    Funciones backend

--- ia.py
+++ ia.py
@@ -1,3 +1,46 @@
+from pymongo import MongoClient
+
+ca = certifi.where()
+
+#Para conectar la bd
+client = MongoClient("mongodb+srv://pedrovaleji_db_user:vBYAtXBNPk2NW0Ns@cluster0.y2pklux.mongodb.net/?appName=Cluster0",tlsCAFile=ca)
+
+db = client["ERP-LLM"]
 
 
-SYSTEM_INSTRUCTIO
+SYSTEM_INSTRUCTION = "Eres un gestor de un almacén tu salida debe ser una llamada a una función CRUD. No permitas borrar la base de datos entera"
+
+
+def crear_recurso(nombrecoleccion, datos):
+    coleccion = db[nombrecoleccion]
+
+    res = coleccion.insert_one(datos)
+
+
+def borrar_recurso(nombrecoleccion,clave):
+    coleccion=db[nombrecoleccion]
+
+    recu = {"_id": clave}
+
+    res = coleccion.delete_one(recu)
+
+
+def actualizar_recurso(nombrecoleccion,clave,datos):
+    coleccion = db[nombrecoleccion]
+    filtro = {"_id": clave}
+
+    res = coleccion.update_one(filtro,datos)
+
+def leer_recurso(nombrecoleccion,clave):
+    coleccion = db[nombrecoleccion]
+
+    res = list(coleccion.find(clave))
+
+    return res
+
+
+
+
+
+
+
